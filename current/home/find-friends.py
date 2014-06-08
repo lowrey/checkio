9f21b683d563d9fdc57decb3ba731feb5da57e4e
lowrey <bret@lowrey.me>
2014-06-08 20:59:10 +0000

    add find-friends solution

--- current/home/find-friends.py
+++ current/home/find-friends.py
@@ -1,8 +1,26 @@
 def check_connection(network, first, second):
     def create_graph(netw):
+        g = {}
         for rel in netw:
             p1, p2 = rel.split('-')
-    return True or False
+            if p1 in g:
+                g[p1] += [p2]
+            else:
+                g[p1] = [p2]
+            if p2 in g:
+                g[p2] += [p1]
+            else:
+                g[p2] = [p1]
+        return g
+    def find_path(graph, n1, n2, path=[]):
+        path += [n1]
+        if n1 == n2:
+            return path
+        for n in graph[n1]:
+            if n not in path:
+                return find_path(graph, n, n2, path)
+        return None
+    return find_path(create_graph(network), first, second) != None
 
 
 if __name__ == '__main__':
